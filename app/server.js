79df2b0cdc798068a72f06e73357423c37a6b1ac
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-09-15 20:09:17 +0330

    application class coded

---
+++ app/server.js
@@ -0,0 +1,63 @@
+module.exports = class Application {
+  #express = require("express");
+  #app = this.#express();
+  constructor(PORT, DB_URL) {
+    this.configDatabase(DB_URL);
+    this.configApplication();
+    this.createServer(PORT);
+    this.createRoutes();
+    this.errorhandler();
+  }
+  configApplication() {
+    const path = require("path");
+    this.#app.use(this.#express.json());
+    this.#app.use(this.#express.urlencoded({ extended: true }));
+    this.#app.use(this.#express.static(path.join(__dirname, ".." + "/public")));
+  }
+
+  createServer(PORT) {
+    const http = require("http");
+    const server = http.createServer(this.#app);
+    server.listen(PORT, () => {
+      console.log("server run on " + PORT + " port http://localhost:" + PORT);
+    });
+  }
+
+  async configDatabase(DB_URL) {
+    const mongoose = require("mongoose");
+    if (mongoose.connections[0].readyState) return;
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(DB_URL);
+    console.log("db connected");
+  }
+
+  errorhandler() {
+    this.#app.use((req, res, next) => {
+      return res.status(404).json({
+        status: 404,
+        success: false,
+        message: "صفحه مورد نظر یافت نشد",
+      });
+    });
+    this.#app.use((err, req, res, next) => {
+      const status = err?.status || err?.statusCode || 500;
+      const message = err?.message || "internal server error";
+      return res.status(404).json({
+        status,
+        success: false,
+        message,
+      });
+    });
+  }
+
+  createRoutes() {
+    this.#app.get("/", (req, res) => {
+      return (
+        res.status(200).
+        json({
+          message: "new express app",
+        })
+      );
+    });
+  }
+};
